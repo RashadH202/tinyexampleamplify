5e1ebb57f5e0263a5ff3488958f4888b16f1c608
Rashadtheone <rashad.hammonds0114@gmail.com>
2024-04-25 10:34:05 -0400

    local code.

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "tinyexampleamplifye96a80fd": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
